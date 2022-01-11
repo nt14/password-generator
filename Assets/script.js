2c462d6c4e731f020996ebc1dafdd27a550779a5
NT14 <n_tahar@outlook.com>
2022-01-11 23:11:27 +0000

    i added a prompt of how many characters

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+
+  var confrimLength = (prompt("How many Characters do you want in your password? Choose between 8-128 Characters."));
+  //if outside requirment
+  while(confrimLength < 8 || confrimLength > 128) {
+    alert("Password length must be between 8-128 characters long! Try again");
+    var confrimLength = (prompt("How many Characters do you want in your password? Choose between 8-128 Characters."));
+  };
+
+  
+
+}
+
+ 
+
+
+
 
 // Write password to the #password input
-function writePassword() {
+function writePassword() { 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
